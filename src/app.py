8e2bf948e1059b225379f8562586579a6eff1517
NickOsipov <nick.osipov.91@gmail.com>
2024-10-11 01:40:30 +0300

    feat: app.py

--- src/app.py
+++ src/app.py
@@ -5,22 +5,23 @@ Application
 import os
 
 from fastapi import FastAPI, HTTPException
-from loguru import logger
 from pydantic import BaseModel
 import pandas as pd
+from loguru import logger
 
 from src.inference import load_model, predict
 
-# Load model
+
 logger.info("Loading model")
 MODEL_PATH = os.path.join("models", "model.joblib")
 MODEL = load_model(MODEL_PATH)
 logger.info("Model loaded")
 
+
 app = FastAPI()
 
 class IrisFeatures(BaseModel):
-    """Features for Iris dataset"""
+    """Iris features"""
     sepal_length: float
     sepal_width: float
     petal_length: float
@@ -31,6 +32,7 @@ def health_check() -> dict:
     """Health check"""
     return {"status": "ok"}
 
+
 @app.post("/predict")
 def make_prediction(features: IrisFeatures) -> dict:
     """Make a prediction by model"""
@@ -39,9 +41,18 @@ def make_prediction(features: IrisFeatures) -> dict:
         prediction = predict(MODEL, data)
         classes = ["setosa", "versicolor", "virginica"]
         pred_class = classes[prediction[0]]
-        logger.info(f"Prediction: {pred_class}")
     except Exception as e:
         logger.error(f"Prediction error: {e}")
-        raise HTTPException(status_code=500, detail="Prediction error")
+        raise HTTPException(
+            status_code=500, 
+            detail="An error occurred during prediction"
+        )
     
     return {"prediction": pred_class}
+
+# {
+#     "sepal_length": 5.1,
+#     "sepal_width": 3.3,
+#     "petal_length": 1.7,
+#     "petal_width": 0.5
+# }
