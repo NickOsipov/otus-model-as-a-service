ced4242215eb45cd1ed48d9473aa690f8e5b321f
NickOsipov <nick.osipov.91@gmail.com>
2025-08-13 22:01:29 +0300

    fix: result json

--- src/app.py
+++ src/app.py
@@ -49,7 +49,7 @@ def make_prediction(features: IrisFeatures) -> dict:
             detail="An error occurred during prediction"
         )
     
-    return {"prediction_class": pred_class}
+    return {"prediction": pred_class}
 
 # {
 #     "sepal_length": 5.1,
